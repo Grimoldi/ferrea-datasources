62cda9ae63fa1e9d60ea87c0a5c76b2deb8b5912
Grimoldi Eugenio <11293040+Grimoldi@users.noreply.github.com>
2025-05-03 14:25:46 +0200

    Corrected comment and name of tests.

--- tests/operations/test_service_openlibrary.py
+++ tests/operations/test_service_openlibrary.py
@@ -7,7 +7,7 @@ OPENLIBRARY_API = "http://openlibrary.org/api/books"
 
 
 def test_openlibrary_service() -> None:
-    """Test Google service integration, happy path."""
+    """Test GooOpenLibrary service integration, happy path."""
     ol_api = OpenLibraryAPI(OPENLIBRARY_API)
     ol_service = OpenLibraryService(ol_api)
     data = ol_service.get_author_portrait(ISBN)
@@ -20,8 +20,8 @@ def test_openlibrary_service() -> None:
     assert data == openlibrary_expected_data()
 
 
-def test_google_service_not_found() -> None:
-    """Test Google service integration, author not found."""
+def test_openlibrary_service_not_found() -> None:
+    """Test GooOpenLibrary service integration, author not found."""
     DEFINITELY_NOT_AN_ISBN = "404"
 
     ol_api = OpenLibraryAPI(OPENLIBRARY_API)
