69e9451cce7127ec20c62d2b23d11f08649c0e05
Grimoldi Eugenio <11293040+Grimoldi@users.noreply.github.com>
2025-06-08 22:47:33 +0200

    Moved under bastion the run of the web server.

--- src/app.py
+++ src/app.py
@@ -1,12 +1,23 @@
 from pathlib import Path
 
+import uvicorn
 from fastapi import FastAPI
 from ferrea.core.oas import add_openapi_schema
 
+from configs import settings
 from routers import datasources
 
-models_path = Path("/oas/bundle.yaml")
-app = FastAPI()
-app = add_openapi_schema(app, models_path)
 
-app.include_router(datasources.router)
+def app() -> FastAPI:
+    """Setup the app with custom logic."""
+    app = FastAPI()
+    if settings.ferrea_app.oas_path is not None:
+        app = add_openapi_schema(app, Path(settings.ferrea_app.oas_path))
+
+    app.include_router(datasources.router)
+
+    return app
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", port=8080, log_level=None, factory=True, access_log=False)
