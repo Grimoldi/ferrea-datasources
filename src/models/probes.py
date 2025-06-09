0dae5d728dbd3bf4c48fe0c058ed5e34cefc8479
Grimoldi Eugenio <11293040+Grimoldi@users.noreply.github.com>
2025-06-09 15:39:09 +0200

    Added a bunch of docstrings.

--- src/models/probes.py
+++ src/models/probes.py
@@ -6,19 +6,22 @@ from pydantic import BaseModel, Field
 
 
 class HealthProbe(BaseModel):
+    """Health Probe object. Holds the overall status and each entity checked."""
+
     status: HealthStatus
     entities: list[Entity]
 
 
 class Entity(BaseModel):
+    """Entity object. Holds the status for a single entity checked."""
+
     name: str
     status: HealthStatus
     internal_status: bool = Field(exclude=True)
 
-    def get_status(self) -> bool:
-        return self.internal_status
-
 
 class HealthStatus(StrEnum):
+    """Simple enum for status."""
+
     HEALTHY = auto()
     UNHEALTHY = auto()
